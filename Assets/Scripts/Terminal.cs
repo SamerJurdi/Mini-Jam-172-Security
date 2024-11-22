dd10d4a9aad2fb197376c406cd25fc375db25261
Denis <133595385+denis002gd@users.noreply.github.com>
2024-11-22 16:37:17 +0200

    hus

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Terminal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2 screenSpace;
+    public float xAxis;
+    public float yAxis;
+    public float distance;
+    public GameObject text;
     void Start()
     {
         
@@ -13,6 +17,13 @@ public class Terminal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        screenSpace = new Vector2(xAxis, yAxis);
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Instantiate(text, screenSpace, Quaternion.identity);
+            screenSpace = new Vector2(xAxis, yAxis);
+            yAxis -= distance;
+        }
     }
 }
