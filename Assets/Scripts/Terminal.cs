e8f9661abd9ea25da8b7268d1638bd03b8b842a5
Denis <133595385+denis002gd@users.noreply.github.com>
2024-11-22 19:13:14 +0200

    added stuff
    now you can click and drag the terminal

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -8,10 +8,13 @@ public class Terminal : MonoBehaviour
     public static GameObject[] total;
     public float xAxis;
     public float yAxis;
-    public float distance = 1f; 
+    public float distance = 1f;
     public GameObject[] inputThing;
     private int maxObj = 15;
-    private int obj = 0; 
+    private int obj = 0;
+
+    private bool isDragging = false;
+    private Vector3 offset;
 
     void Start()
     {
@@ -20,7 +23,7 @@ public class Terminal : MonoBehaviour
 
     void Update()
     {
-       
+   
         if (Input.GetKeyDown(KeyCode.U))
         {
             AddRow(inputThing[0]);
@@ -30,59 +33,65 @@ public class Terminal : MonoBehaviour
         {
             AddRow(inputThing[1]);
         }
+
+  
+        if (isDragging)
+        {
+            DragTerminal();
+        }
     }
 
     private void AddRow(GameObject prefab)
     {
-    
+        
         if (obj >= maxObj)
         {
             RemoveFirstRow();
         }
 
-   
-        Vector2 newPosition = new Vector2(xAxis, -obj * distance);
+      
+        Vector2 newPosition = new Vector2(transform.position.x + xAxis, transform.position.y + (-obj * distance) - 1.6f);
 
-        // Instantiate the new object
+     
         GameObject g = Instantiate(prefab, newPosition, Quaternion.identity);
-         total[obj] = g;
+        g.transform.parent = transform; 
 
-   
+        total[obj] = g;
         obj++;
     }
 
     private void RemoveFirstRow()
     {
- 
+    
         if (total[0] != null)
         {
             Destroy(total[0]);
         }
 
-    
+      
         for (int i = 1; i < maxObj; i++)
         {
             total[i - 1] = total[i];
         }
 
-
+       
         total[maxObj - 1] = null;
+
 
         for (int i = 0; i < maxObj; i++)
         {
             if (total[i] != null)
             {
-                total[i].transform.position = new Vector2(xAxis, -i * distance);
+                total[i].transform.position = new Vector2(transform.position.x + xAxis, transform.position.y + (-i * distance) - 1.6f);
             }
         }
-
 
         obj--;
     }
 
     private void RemoveAllRows()
     {
-
+     
         for (int i = 0; i < maxObj; i++)
         {
             if (total[i] != null)
@@ -91,7 +100,47 @@ public class Terminal : MonoBehaviour
             }
         }
 
+   
         total = new GameObject[maxObj];
         obj = 0;
+    }
+
+  
+    private void OnMouseDown()
+    {
+        isDragging = true;
+
+       
+        offset = transform.position - GetMouseWorldPosition();
+    }
+
+   
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+   
+    private void DragTerminal()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+        transform.position = mousePosition + offset;
+
+   
+        for (int i = 0; i < obj; i++)
+        {
+            if (total[i] != null)
+            {
+                total[i].transform.position = new Vector2(transform.position.x + xAxis, transform.position.y + (-i * distance) - 1.6f);
+            }
+        }
+    }
+
+  
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Camera.main.WorldToScreenPoint(transform.position).z; 
+        return Camera.main.ScreenToWorldPoint(mouseScreenPosition);
     }
 }
